87d6f17718b11ab53b0fa2c3107b142b519bba51
petya <0vasip34@solent.ac.uk>
2026-01-19 20:00:14 +0000

    j

---
+++ PythonProject2 week 1/name.py
@@ -0,0 +1,4 @@
+#Ask what is my name
+print ("What is your name?")
+name = input()
+print("My name is {Petya}")
---
+++ PythonProject3/if_statement.py
@@ -0,0 +1,9 @@
+book = input
+print("Enter type of book:")
+if book == "adventure":
+    print("I like adventure books!")
+print()
+print("Finished reading book!")
+
+
+
